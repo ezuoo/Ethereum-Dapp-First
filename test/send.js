46c7be1de976e6bc0b53675ca585b363bab3ceb8
juho0428@naver.com <juho0428@naver.com>
2021-04-14 12:41:34 +0900

    create smart contract

--- test/send.js
+++ test/send.js
@@ -1,6 +1,6 @@
 module.exports = function(callback) {
     web3.eth.sendTransaction({
-        from: "0x6DEC5f859AB5eA5F00deEeB97f47110C0Ce33412", 
+        from: "0x89d6815fA033e87fcBE97f793F0ef426A5A057d7", 
         to:"0x446ABF19A3d667323aBbe575334073949D76Fb13",
         value: web3.utils.toWei("30")}, 
         callback);
